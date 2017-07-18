c42952b4337d1a018302b0fd54797a4fda1d2760
Michiel de Vos <michieldevos@MacBook-Pro-van-Michiel.local>
2017-07-18 11:32:05 +0200

    Added firstname and lastname to devise user model and implemented this in sign up process

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,10 @@ User.destroy_all
 
 user1 = User.create!(email: 'tom@cat.com', password: '123456', admin: true)
 
-user2 = User.create!(email: 'tommy@cat2.com', password: '123456', admin: false)
-user3 = User.create!(email: 'tommy@cat3.com', password: '123456', admin: false)
-user4 = User.create!(email: 'tommy@cat4.com', password: '123456', admin: false)
-user5 = User.create!(email: 'tommy@cat5.com', password: '123456', admin: false)
-user6 = User.create!(email: 'tommy@cat6.com', password: '123456', admin: false)
-user7 = User.create!(email: 'tommy@cat7.com', password: '123456', admin: false)
-user8 = User.create!(email: 'tommy@cat8.com', password: '123456', admin: false)
+user2 = User.create!(first_name: Faker::Name.first_name , last_name: Faker::Name.last_name ,email: 'tommy@cat2.com', password: '123456', admin: false)
+user3 = User.create!(first_name: Faker::Name.first_name , last_name: Faker::Name.last_name ,email: 'tommy@cat3.com', password: '123456', admin: false)
+user4 = User.create!(first_name: Faker::Name.first_name , last_name: Faker::Name.last_name ,email: 'tommy@cat4.com', password: '123456', admin: false)
+user5 = User.create!(first_name: Faker::Name.first_name , last_name: Faker::Name.last_name ,email: 'tommy@cat5.com', password: '123456', admin: false)
+user6 = User.create!(first_name: Faker::Name.first_name , last_name: Faker::Name.last_name ,email: 'tommy@cat6.com', password: '123456', admin: false)
+user7 = User.create!(first_name: Faker::Name.first_name , last_name: Faker::Name.last_name ,email: 'tommy@cat7.com', password: '123456', admin: false)
+user8 = User.create!(first_name: Faker::Name.first_name , last_name: Faker::Name.last_name ,email: 'tommy@cat8.com', password: '123456', admin: false)
