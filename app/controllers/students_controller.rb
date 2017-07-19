8b8913406a504fcbab627d70f6320a4d7b91e2e8
Michiel de Vos <michieldevos@MacBook-Pro-van-Michiel.local>
2017-07-19 13:40:10 +0200

    Fiurst working version algorythm

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,17 +2,10 @@ class StudentsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @students = User.all_students.to_a
-    @pairs = initial_matches
-
+    pairing = Pairing.new
+    @pairs = pairing.create_pairs
   end
 
   private
 
-
-  # def opportunities
-  #   self.select { |studentid| Pair.previous_matches.each {|matched| return false if matched.matched_id == studentid} }
-  #   @opportunities = @students.select { |studentid| Pair.previous_matches.each {|matched| return false if matched.matched_id == studentid} }
-  # end
-  # @student = @students.shift
 end
