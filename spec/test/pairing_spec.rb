f42486bf761e05769fa5f21c90a1d43c8f6cd112
DKateryna <derkach.kateryna@gmail.com>
2017-07-20 14:17:36 +0200

    Test for 4 students

--- spec/test/pairing_spec.rb
+++ spec/test/pairing_spec.rb
@@ -32,3 +32,18 @@ describe "Should pair three students" do
     expect(result.length).to be(1)
   end
 end
+
+describe "Should pair four students" do
+  let!(:user1) { create :user, first_name: "Doe", email: "a@t.com", password: "a@w.com", admin: false}
+  let!(:user2) { create :user, first_name: "Joe", email: "b@w.com", password: "s@w.com", admin: false}
+  let!(:user3) { create :user, first_name: "Joe", email: "c@w.com", password: "d@w.com", admin: false}
+  let!(:user4) { create :user, first_name: "Joe", email: "d@w.com", password: "d@w.com", admin: false}
+
+  let! (:match1) {create :pair, user: user1, matched_id: user2.id}
+  let! (:match2) {create :pair, user: user3, matched_id: user4.id}
+
+  it "works for 4 students" do
+    result = Pair.create_pairs
+    expect(result).not_to contain_exactly(match1, match2)
+  end
+end
