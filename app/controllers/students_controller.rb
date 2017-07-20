8c347a9d3ea9059ec1b778a66ef08b0e17b4b8e1
Michiel de Vos <michieldevos@MacBook-Pro-van-Michiel.local>
2017-07-20 17:25:30 +0200

    Student view

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,5 +2,6 @@ class StudentsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @pairs = Pair.where("user = '?' OR matched_id = '?'", current_user.id, current_user.id)
   end
 end
