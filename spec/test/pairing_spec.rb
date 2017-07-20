69a6f21bfd225b5b03a7de4cce1f7e0f33b98738
DKateryna <derkach.kateryna@gmail.com>
2017-07-20 10:01:01 +0200

    Test for one student

--- spec/test/pairing_spec.rb
+++ spec/test/pairing_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
+describe "Should pair one students" do
+  let!(:user) { create :user, first_name: "Doe", email: "a@t.com", password: "b@w.com", admin: false}
+
+  it "works for 1 students" do
+
+    result = Pair.new.create_pairs
+    expect(result.length).to be(1)
+  end
+end
+
 describe "Should pair two students" do
   let!(:user1) { create :user, first_name: "Doe", email: "a@t.com", password: "b@w.com", admin: false}
   let!(:user2) { create :user, first_name: "Joe", email: "b@w.com", password: "b@w.com", admin: false}
